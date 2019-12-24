be51b771204d78f4676b2811678e1b3bf41489f9
Papp Imre <imi950127@gmail.com>
2019-12-24 13:04:08 +0100

    setup

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class='navbar bg-primary'>
+        <ul>
+            <li>
+                <a>Activities</a>
+            </li>
+            <li>
+                <a>Mylist</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
